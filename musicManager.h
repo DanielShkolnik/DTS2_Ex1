36ceb153aec38098ea8cc106febd2d9c059a36aa
DanielShkolnik <DanielShkolnik@users.noreply.github.com>
2020-04-28 16:16:27 +0300

    new

--- musicManager.h
+++ musicManager.h
@@ -11,18 +11,30 @@
 #include "disc.h"
 #include "song.h"
 #include "library1.h"
+#include "exception"
 
 
 
 class MusicManager{
 private:
 Avl<int,Artist> artistTree;
-Node<int,Avl<int,Disc>> bestHitsListStart;
-Node<int,Avl<int,Disc>> bestHitsListFinish;
+Node<int,Avl<int,Disc>>* bestHitsListStart;
+Node<int,Avl<int,Disc>>* bestHitsListFinish;
 
 public:
-    MusicManager() = default;
-    ~MusicManager() = default;
+    MusicManager(){
+        this->bestHitsListStart = new Node<int,Avl<int,Disc>>;
+        this->bestHitsListFinish = this->bestHitsListStart;
+    }
+    ~MusicManager(){
+        Node<int,Avl<int,Disc>>* current = this->bestHitsListStart;
+        Node<int,Avl<int,Disc>>* prev = current;
+        while(current!= nullptr){
+            current= current->getNext();
+            delete prev;
+            prev = current;
+        }
+    }
     MusicManager(const MusicManager& musicManager) = delete;
     MusicManager& operator=(const MusicManager& musicManager) = delete;
 
@@ -32,7 +44,22 @@ public:
     }
 
     StatusType RemoveArtist(int artistID){
+        if(artistID<=0) return INVALID_INPUT;
+        try{
+            Artist* artist= this->artistTree.find(artistID)->getData();
+            this->artistTree.deleteVertice(artistID);
+            Node<int,Avl<int,Disc>> current = this->bestHitsListStart;
+            while(current!= nullptr){
 
+            }
+
+        }
+        catch(std::bad_alloc& e) {
+            return ALLOCATION_ERROR;
+        }
+        catch(Avl<int,Artist>::KeyNotFound& e){
+            return FAILURE;
+        }
     }
 
     StatusType AddToSongCount(int artistID, int songID){
