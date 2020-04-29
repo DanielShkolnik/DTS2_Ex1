6834429b2b5972f350ca44352ec79fbd3d44d83d
DanielShkolnik <DanielShkolnik@users.noreply.github.com>
2020-04-29 11:06:37 +0300

    new

--- library1.cpp
+++ library1.cpp
@@ -35,5 +35,6 @@ StatusType GetRecommendedSongs(void *DS, int numOfSongs, int *artists, int *song
 }
 
 void Quit(void** DS){
-    ((MusicManager*)DS)
+    delete *(MusicManager **)DS;
+    *DS = nullptr;
 }
