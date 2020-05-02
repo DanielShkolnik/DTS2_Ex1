9044d5d0c41474f2a5a16a6f63bcd23cda115308
DanielShkolnik <DanielShkolnik@users.noreply.github.com>
2020-05-02 10:17:13 +0300

    last_version

--- artist.h
+++ artist.h
@@ -13,22 +13,19 @@ private:
     int artistID;
     int numOfSongs;
     Song** songArray;
-    Node<int,Disc>** discNodeArray;
-    int indexDiscArray;
 public:
-    Artist(int artistID,int numOfSongs):artistID(artistID),numOfSongs(numOfSongs),indexDiscArray(0){
+    Artist(int artistID,int numOfSongs):artistID(artistID),numOfSongs(numOfSongs){
         this->songArray = new Song*[numOfSongs]();
         for (int i = 0; i < this->numOfSongs; ++i) {
             songArray[i] = new Song(i, this->artistID);
         }
-        this->discNodeArray = new Node<int,Disc>*[numOfSongs]();
+
     };
     ~Artist(){
         for (int i = 0; i < numOfSongs; ++i) {
             delete this->songArray[i];
         }
         delete[] songArray;
-        delete [] discNodeArray;
     };
     Artist(const Artist& artist)= delete;
     Artist& operator=(const Artist& artist)= delete;
@@ -39,28 +36,12 @@ public:
         return this->artistID;
     }
 
-    void initNewDiscNode(Node<int,Disc>* disc){
-        this->discNodeArray[this->indexDiscArray] = disc;
-        this->indexDiscArray++;
-    }
-
-    void setDiscNode(int index,Node<int,Disc>* disc){
-        this->discNodeArray[index] = disc;
-    }
-
-    Node<int,Disc>* getDiscNode(int index){
-        return this->discNodeArray[index];
-    }
-
     Song* getSong(int index){
         return this->songArray[index];
     }
 
     int getNumOfSongs() const {
         return this->numOfSongs;
-    }
-    int getLastDiscIndex(){
-        return this->indexDiscArray-1;
     }
 
     class INVALID_INPUT{};
